cadb615a12136f539da081587e26cf96bf836773
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-20 21:22:23 -0300

    variaveis e constructor dto

--- src/main/java/com/cdb/estoque/dto/GameDTO.java
+++ src/main/java/com/cdb/estoque/dto/GameDTO.java
@@ -1,4 +1,31 @@
 package com.cdb.estoque.dto;
 
+import com.cdb.estoque.entity.Game;
+
 public class GameDTO {
+    private Long id;
+    private String titleGame;
+    private String plataform;
+    private Double price;
+    private Integer stock;
+
+    public GameDTO(){
+
+    }
+
+    public GameDTO(Long id, String titleGame, String plataform, Double price, Integer stock) {
+        this.id = id;
+        this.titleGame = titleGame;
+        this.plataform = plataform;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public GameDTO(Game game) {
+        this.id = game.getId();
+        this.titleGame = game.getTitleGame();
+        this.plataform = game.getPlataform();
+        this.price = game.getPrice();
+        this.stock = game.getStock();
+    }
 }
