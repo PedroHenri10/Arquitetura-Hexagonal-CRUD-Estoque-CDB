5114f9aa18b4c77ec04095b97f4ff975fd6275f9
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-20 21:05:57 -0300

    Getters and setters + constructors criados

--- src/main/java/com/cdb/estoque/entity/Game.java
+++ src/main/java/com/cdb/estoque/entity/Game.java
@@ -26,5 +26,51 @@ public class Game {
 
     }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitleGame() {
+        return titleGame;
+    }
+
+    public void setTitleGame(String titleGame) {
+        this.titleGame = titleGame;
+    }
+
+    public String getPlataform() {
+        return plataform;
+    }
+
+    public void setPlataform(String plataform) {
+        this.plataform = plataform;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Game(Long id, String titleGame, String plataform, Double price, Integer stock) {
+        this.id = id;
+        this.titleGame = titleGame;
+        this.plataform = plataform;
+        this.price = price;
+        this.stock = stock;
+    }
 }
