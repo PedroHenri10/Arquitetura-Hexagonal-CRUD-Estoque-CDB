adc69ab7fc9ecde261d17f1c3f368f1bfb45bad2
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-21 22:58:25 -0300

    listando tudo controller

--- src/main/java/com/cdb/estoque/controller/GameController.java
+++ src/main/java/com/cdb/estoque/controller/GameController.java
@@ -1,9 +1,13 @@
 package com.cdb.estoque.controller;
 
+import com.cdb.estoque.dto.GameDTO;
 import com.cdb.estoque.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/games")
@@ -12,5 +16,8 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
-    
+    @GetMapping
+    public List<GameDTO> getAllGames(){
+        return gameService.listAll();
+    }
 }
