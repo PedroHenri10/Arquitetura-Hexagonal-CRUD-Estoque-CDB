1232179873e5126d1d9e654f1693ea9f82b1edbe
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-21 22:50:46 -0300

    ESTRUTURA INICIAL

--- src/main/java/com/cdb/estoque/controller/GameController.java
+++ src/main/java/com/cdb/estoque/controller/GameController.java
@@ -1,4 +1,16 @@
 package com.cdb.estoque.controller;
 
+import com.cdb.estoque.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/games")
 public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    
 }
