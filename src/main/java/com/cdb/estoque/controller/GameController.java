1efd07df5ba8af5cc926c1eb9318bb2602ba3257
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-22 23:08:16 -0300

    acreate game controller function

--- src/main/java/com/cdb/estoque/controller/GameController.java
+++ src/main/java/com/cdb/estoque/controller/GameController.java
@@ -3,9 +3,8 @@ package com.cdb.estoque.controller;
 import com.cdb.estoque.dto.GameDTO;
 import com.cdb.estoque.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +19,10 @@ public class GameController {
     public List<GameDTO> getAllGames(){
         return gameService.listAll();
     }
+
+    @PostMapping
+    public ResponseEntity<GameDTO> createGame(@RequestBody GameDTO dto) {
+        return ResponseEntity.ok(gameService.save(dto));
+    }
 }
+
