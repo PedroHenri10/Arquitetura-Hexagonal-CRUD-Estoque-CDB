dd89fcc684bf70db99aaf41908b1f78ba43448ca
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-08-27 20:49:55 -0300

    gameInpuPort funcoes do crud

--- src/main/java/com/cdb/estoque/port/input/GameInputPort.java
+++ src/main/java/com/cdb/estoque/port/input/GameInputPort.java
@@ -1,4 +1,10 @@
 package com.cdb.estoque.port.input;
 
 public interface GameInputPort {
+
+    List<Game> findAll();
+    Optional<Game> findById(long id);
+    Game save (Game game);
+    Game update(Long id, Game game);
+    void deleteById(Long id);
 }
