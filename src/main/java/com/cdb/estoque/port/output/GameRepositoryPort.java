e3a0b0434a7f13c6eea2ddf1ae6dcaa94d46709e
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-08-28 21:42:26 -0300

    implementacao repository no output

--- src/main/java/com/cdb/estoque/port/output/GameRepositoryPort.java
+++ src/main/java/com/cdb/estoque/port/output/GameRepositoryPort.java
@@ -1,4 +1,15 @@
 package com.cdb.estoque.port.output;
 
+import com.cdb.estoque.core.domain.model.Game;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface GameRepositoryPort {
+    List<Game> findAll();
+    Optional<Game> findById(Long id);
+    Game save (Game game);
+    Game update(Long id, Game game);
+    void deleteById(Long id);
+    boolean existsById(Long id);
 }
