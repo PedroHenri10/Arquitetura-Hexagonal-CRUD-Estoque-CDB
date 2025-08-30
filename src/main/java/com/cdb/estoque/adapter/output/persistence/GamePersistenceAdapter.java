374f6cef0696b6849a28937d7718645eba20b6b1
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-08-29 22:06:24 -0300

    metodos finds persistence

--- src/main/java/com/cdb/estoque/adapter/output/persistence/GamePersistenceAdapter.java
+++ src/main/java/com/cdb/estoque/adapter/output/persistence/GamePersistenceAdapter.java
@@ -2,22 +2,34 @@ package com.cdb.estoque.adapter.output.persistence;
 
 import com.cdb.estoque.core.domain.model.Game;
 import com.cdb.estoque.port.output.GameRepositoryPort;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Component
+@RequiredArgsConstructor
 public class GamePersistenceAdapter implements GameRepositoryPort {
+
+    private final GameJpaRepository gameJpaRepository;
+    private final GamePersistenceMapper gamePersistenceMapper;
 
     @Override
     public List<Game> findAll() {
-        return List.of();
+
+        List<GameEntity> allEntities = gameJpaRepository.findAll();
+        return allEntities.stream().map(mapper::toDomain).collect(Collectors.toList());
     }
 
     @Override
     public Optional<Game> findById(Long id) {
-        return Optional.empty();
+
+        Optional<GameEntity> entityOptional = gameJpaRepository.findById(id);
+        return entityOptional.map(mappper::toDomain);
     }
 
-    
+
 }
