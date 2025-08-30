3578b3c2482f3878b906048c37ae49d1a2530e95
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-08-30 13:01:51 -0300

    post refatorado

--- src/main/java/com/cdb/estoque/adapter/input/controller/GameController.java
+++ src/main/java/com/cdb/estoque/adapter/input/controller/GameController.java
@@ -3,12 +3,15 @@ package com.cdb.estoque.adapter.input.controller;
 import com.cdb.estoque.adapter.input.mapper.GameRestMapper;
 import com.cdb.estoque.adapter.input.request.GameRequest;
 import com.cdb.estoque.adapter.input.response.GameResponse;
+import com.cdb.estoque.core.domain.model.Game;
+import com.cdb.estoque.exception.ResourceNotFoundException;
 import com.cdb.estoque.port.input.GameInputPort;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,13 +24,13 @@ public class GameController {
     private final GameRestMapper mapper;
 
     @GetMapping
-    public ResponseEntity<List<GameRequest>> getAllGames(){
+    public ResponseEntity<List<GameResponse>> getAllGames(){
         List<GameResponse> allGames = gameInputPort.findAll().stream().map(mapper::toResponse).collect(Collectors.toList());
         return ResponseEntity.ok(allGames);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GameRequest> getGameById(@PathVariable Long id) {
+    public ResponseEntity<GameResponse> findById(@PathVariable Long id) {
         return gameInputPort.findById(id)
                 .map(mapper::toResponse)
                 .map(ResponseEntity::ok);
@@ -36,14 +39,14 @@ public class GameController {
     }
 
     @PostMapping
-    public ResponseEntity<GameRequest> createGame(@Valid @RequestBody GameRequest request) {
-        var gameDomain = mapper.toDomain(request);
-        var savedGame = gameInputPort.save(gameDomain);
-        return ResponseEntity.status(201).body(mapper.toResponse(savedGame));
+    public ResponseEntity<GameResponse> createGame(@Valid @RequestBody GameRequest request) {
+       Game toSave = mapper.toDomain(request);
+       Game saved = gameInputPort.save(toSave);
+        return ResponseEntity.created(URI.create("/games/" + saved.getId())).body(mapper.toResponse(savedGame));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<GameRequest> updateGame(@PathVariable Long id, @Valid @RequestBody GameRequest game) {
+    public ResponseEntity<GameResponse> updateGame(@PathVariable Long id, @Valid @RequestBody GameRequest game) {
         var gameToUpdate = mapper.toDomain(request);
         var updatedGame = gameInputPort.update(id, gameToUpdate);
         return ResponseEntity.ok(mapper.toResponse(updated game));
