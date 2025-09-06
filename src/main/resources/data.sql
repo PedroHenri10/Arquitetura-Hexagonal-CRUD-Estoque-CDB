d140b4a926e77da71f477f5e0279b061c4ab2ce1
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-06 20:51:00 -0300

    deixando so um insert into para sql funcionar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,4 @@
-INSERT INTO games (titleGame, plataform, genre, price, stock) VALUES
-('The Legend of Zelda: Breath of the Wild', 'Nintendo Switch', 'Aventura', 299.90, 10),
-('Super Mario Odyssey', 'Nintendo Switch', 'Plataforma', 249.90, 15),
-('Mario Kart 8 Deluxe', 'Nintendo Switch', 'Corrida', 229.90, 20),
-('Pokémon Sword', 'Nintendo Switch', 'RPG', 279.90, 12),
-('Pokémon Shield', 'Nintendo Switch', 'RPG', 279.90, 10),
-('Metroid Dread', 'Nintendo Switch', 'Ação', 289.90, 8),
-('Animal Crossing: New Horizons', 'Nintendo Switch', 'Simulação', 259.90, 25),
-('Splatoon 2', 'Nintendo Switch', 'Tiro', 239.90, 14),
-('Bayonetta 2', 'Nintendo Switch', 'Ação', 219.90, 6),
-('Fire Emblem: Three Houses', 'Nintendo Switch', 'RPG Tático', 269.90, 7),
-
-INSERT INTO games (titleGame, plataform, genre, price, stock) VALUES
+INSERT INTO games (title_Game, plataform, genre, price, stock) VALUES
 ('The Legend of Zelda: Breath of the Wild', 'Nintendo Switch', 'Aventura', 299.90, 10),
 ('Super Mario Odyssey', 'Nintendo Switch', 'Plataforma', 249.90, 15),
 ('Mario Kart 8 Deluxe', 'Nintendo Switch', 'Corrida', 229.90, 20),
