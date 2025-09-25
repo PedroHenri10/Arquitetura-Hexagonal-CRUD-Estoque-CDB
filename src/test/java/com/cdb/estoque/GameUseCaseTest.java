8f62b00984dea8c0c91ff98e0c2aab469e672fc2
Tarita Lima <tarita36@hotmail.com>
2025-09-25 16:15:04 -0300

    feat: adicionado mais 8 testes unitários e atualizado pom.xml

--- src/test/java/com/cdb/estoque/GameUseCaseTest.java
+++ src/test/java/com/cdb/estoque/GameUseCaseTest.java
@@ -2,12 +2,15 @@ package com.cdb.estoque;
 
 import com.cdb.estoque.core.domain.model.Game;
 import com.cdb.estoque.core.useCase.GameUseCase;
+import com.cdb.estoque.exception.ResourceNotFoundException;
 import com.cdb.estoque.port.output.GameRepositoryPort;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -109,19 +112,106 @@ public class GameUseCaseTest {
     }
 
     @Test
-    void  increaseStock_quantidadeInvalidaLancaExeception() {
+    void increaseStock_lancaExcecaoQuandoZero() {
         Long id = 1L;
         Game game = new Game(id, "Jogo Teste", "PC", "Ação", 150.0, 10);
 
         when(repo.findById(id)).thenReturn(Optional.of(game));
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            gameUseCase.increaseStock(id, 0);
-        });
+        assertThatThrownBy(() -> gameUseCase.increaseStock(id, 0))
+                .isInstanceOf(IllegalArgumentException.class);
 
         verify(repo).findById(id);
         verify(repo, never()).save(any(Game.class));
+    }
+
+    @Test
+    void increaseStock_lancaExcecaoQuandoNegativo() {
+        Long id = 1L;
+        Game game = new Game(id, "Jogo Teste", "PC", "Ação", 150.0, 10);
+
+        when(repo.findById(id)).thenReturn(Optional.of(game));
+
+        assertThatThrownBy(() -> gameUseCase.increaseStock(id, -5))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        verify(repo).findById(id);
+        verify(repo, never()).save(any(Game.class));
+    }
+
+    @Test
+    void decreaseStock_decrementaEstoqueCorretamente(){
+        Long id = 1L;
+        Game game = new Game(id, "Jogo Teste", "PC", "Ação", 150.0, 10);
+
+        when(repo.findById(id)).thenReturn(Optional.of(game));
+
+        gameUseCase.decreaseStock(id, 3);
+
+        assertEquals(7, game.getStock());
+
+        verify(repo).save(game);
 
     }
+
+
+    @Test
+    void decreaseStock_lancaExcecaoQuandoZero() {
+        Long id = 1L;
+        Game game = new Game(id, "Jogo Teste", "PC", "Ação", 150.0, 10);
+
+        when(repo.findById(id)).thenReturn(Optional.of(game));
+
+        assertThatThrownBy(() -> gameUseCase.decreaseStock(id, 0))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        verify(repo).findById(id);
+        verify(repo, never()).save(any(Game.class));
+    }
+
+    @Test
+    void decreaseStock_lancaExcecaoQuandoNegativo() {
+        Long id = 1L;
+        Game game = new Game(id, "Jogo Teste", "PC", "Ação", 150.0, 10);
+
+        when(repo.findById(id)).thenReturn(Optional.of(game));
+
+        assertThatThrownBy(() -> gameUseCase.decreaseStock(id, -5))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        verify(repo).findById(id);
+        verify(repo, never()).save(any(Game.class));
+    }
+
+    @Test
+    void findByTitleGameContainingIgnoreCase_retornaListaQuandoEncontrado(){
+        String titulo ="Jogo";
+        Game game = new Game(1L, "Jogo Teste", "PC", "Ação", 150.0, 10);
+
+        when(repo.findByTitleGameContainingIgnoreCase(titulo)).thenReturn(List.of(game));
+
+
+        List<Game>  resultado = gameUseCase.findByTitleGameContainingIgnoreCase(titulo);
+
+        assertEquals(1, resultado.size());
+        assertEquals("Jogo Teste", resultado.get(0).getTitleGame());
+        verify(repo).findByTitleGameContainingIgnoreCase(titulo);
+
+    }
+
+    @Test
+    void findByTitleGameContainingIgnoreCase_exception() {
+        String titulo ="Inexistente";
+
+        when(repo.findByTitleGameContainingIgnoreCase(titulo))
+                .thenReturn(Collections.emptyList());
+
+        assertThatThrownBy(() -> gameUseCase.findByTitleGameContainingIgnoreCase(titulo))
+                .isInstanceOf(ResourceNotFoundException.class);
+
+        verify(repo).findByTitleGameContainingIgnoreCase(titulo);
+    }
+
+
 
 }
