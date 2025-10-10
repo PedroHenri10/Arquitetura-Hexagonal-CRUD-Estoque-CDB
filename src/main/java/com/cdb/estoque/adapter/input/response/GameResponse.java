bcc601db912e234ece493058a0cfc5cb63d0ce9a
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-10-09 23:08:20 -0300

    game response documentado

--- src/main/java/com/cdb/estoque/adapter/input/response/GameResponse.java
+++ src/main/java/com/cdb/estoque/adapter/input/response/GameResponse.java
@@ -1,10 +1,22 @@
 package com.cdb.estoque.adapter.input.response;
 
-public record GameResponse(
-    Long id,
-    String titleGame,
-    String plataform,
-    String genre,
-    Double price,
-    Integer stock
-){}
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Representa os dados de um game retornado pela API.")
+public class GameResponse {
+
+    @Schema(description = "ID do game", example = "1") Long id,
+    @Schema(description = "Título do game", example = "Eocket League") String titleGame,
+    @Schema(description = "Plataforma do game", example = "PC") String plataform,
+    @Schema(description = "Gênero do game", example = "Esporte") String genre,
+    @Schema(description = "Preço do game", example = "199.99") Double price,
+    @Schema(description = "Quantidade em estoque", example = "25") Integer stock
+}
