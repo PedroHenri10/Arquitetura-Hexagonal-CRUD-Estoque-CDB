708d6bf120da41270f6837468b5e2fa71a04c54b
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-10-10 18:56:28 -0300

    colocando as validacoes no request

--- src/main/java/com/cdb/estoque/adapter/input/request/GameRequest.java
+++ src/main/java/com/cdb/estoque/adapter/input/request/GameRequest.java
@@ -2,9 +2,7 @@
 package com.cdb.estoque.adapter.input.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,20 +12,30 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "Representa os dados de entrada para criação ou atualização de um game.")
+@Schema(description = "DTO de entrada para criação ou atualização de um Game no estoque.")
 public class GameRequest {
-    @Schema(description = "Título do game", example = "Rocket League", required = true)
+
+    @NotBlank(message = "O título do jogo é obrigatório.")
+    @Size(min = 2, max = 100, message = "O título deve ter entre 2 e 100 caracteres.")
+    @Schema(description = "Título completo do jogo.", example = "Final Fantasy VII Rebirth", required = true, minLength = 2, maxLength = 100)
     private String titleGame;
 
-    @Schema(description = "Plataforma do game", example = "PC", required = true)
+    @NotBlank(message = "A plataforma é obrigatória.")
+    @Size(max = 50, message = "A plataforma não deve exceder 50 caracteres.")
+    @Schema(description = "Plataforma na qual o jogo está disponível.", example = "PlayStation 5", required = true, maxLength = 50)
     private String plataform;
 
-    @Schema(description = "Gênero do game", example = "Esporte")
+    @Size(max = 50, message = "O gênero não deve exceder 50 caracteres.")
+    @Schema(description = "Gênero do jogo (ex: RPG, Ação, Aventura).", example = "RPG", maxLength = 50)
     private String genre;
 
-    @Schema(description = "Preço do game", example = "199.99", required = true)
+    @NotNull(message = "O preço é obrigatório.")
+    @Positive(message = "O preço deve ser um valor positivo.")
+    @Schema(description = "Preço de venda do jogo.", example = "299.90", required = true, format = "float")
     private Double price;
 
-    @Schema(description = "Quantidade em estoque", example = "25", required = true)
+    @NotNull(message = "O estoque é obrigatório.")
+    @PositiveOrZero(message = "O estoque não pode ser um número negativo.")
+    @Schema(description = "Quantidade de unidades do jogo disponíveis em estoque.", example = "50", required = true, format = "int")
     private Integer stock;
 }
