2f1f7ab9721b39a9399fd1d12595c4bc3f959d25
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-10-11 00:21:27 -0300

    Bean dos padrões

--- src/main/java/com/cdb/estoque/infrastructure/GameUseCaseBean.java
+++ src/main/java/com/cdb/estoque/infrastructure/GameUseCaseBean.java
@@ -1,5 +1,8 @@
 package com.cdb.estoque.infrastructure;
 
+import com.cdb.estoque.core.domain.factory.GameFactory;
+import com.cdb.estoque.core.domain.strategy.DecreaseStockOperation;
+import com.cdb.estoque.core.domain.strategy.IncreaseStockOperation;
 import com.cdb.estoque.core.useCase.GameUseCase;
 import com.cdb.estoque.port.input.GameInputPort;
 import com.cdb.estoque.port.output.GameRepositoryPort;
@@ -10,7 +13,25 @@ import org.springframework.context.annotation.Configuration;
 public class GameUseCaseBean {
 
     @Bean
-    public GameInputPort gameInputPort(GameRepositoryPort gameRepositoryPort) {
-        return new GameUseCase(gameRepositoryPort);
+    public GameInputPort gameInputPort(GameRepositoryPort gameRepositoryPort,
+                                       IncreaseStockOperation increaseStockOperation,
+                                       DecreaseStockOperation decreaseStockOperation,
+                                       GameFactory gameFactory) {
+        return new GameUseCase(gameRepositoryPort, increaseStockOperation, decreaseStockOperation, gameFactory);
+    }
+
+    @Bean
+    public IncreaseStockOperation increaseStockOperation() {
+        return new IncreaseStockOperation();
+    }
+
+    @Bean
+    public DecreaseStockOperation decreaseStockOperation() {
+        return new DecreaseStockOperation();
+    }
+
+    @Bean
+    public GameFactory gameFactory() { 
+        return new GameFactory();
     }
 }
