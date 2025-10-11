7070d421e48fc827d259e2b15fb76dd81f214b8e
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-10-11 14:16:41 -0300

    bean decorator

--- src/main/java/com/cdb/estoque/infrastructure/GameUseCaseBean.java
+++ src/main/java/com/cdb/estoque/infrastructure/GameUseCaseBean.java
@@ -2,17 +2,20 @@ package com.cdb.estoque.infrastructure;
 
 import com.cdb.estoque.core.domain.strategy.DecreaseStockOperation;
 import com.cdb.estoque.core.domain.strategy.IncreaseStockOperation;
+import com.cdb.estoque.core.domain.decorator.IdValidationGameUseCaseDecorator;
+import com.cdb.estoque.core.domain.decorator.LoggingGameUseCaseDecorator;
 import com.cdb.estoque.core.useCase.GameUseCase;
 import com.cdb.estoque.port.input.GameInputPort;
 import com.cdb.estoque.port.output.GameRepositoryPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GameUseCaseBean {
 
     @Bean
-    public GameInputPort gameInputPort(GameRepositoryPort gameRepositoryPort,
+    public GameUseCase gameCoreUseCase(GameRepositoryPort gameRepositoryPort,
                                        IncreaseStockOperation increaseStockOperation,
                                        DecreaseStockOperation decreaseStockOperation) {
         return new GameUseCase(gameRepositoryPort, increaseStockOperation, decreaseStockOperation);
@@ -28,4 +31,10 @@ public class GameUseCaseBean {
         return new DecreaseStockOperation();
     }
 
+    @Bean
+    @Primary
+    public GameInputPort gameInputPort(GameUseCase gameCoreUseCase) {
+        GameInputPort useCaseWithIdValidation = new IdValidationGameUseCaseDecorator(gameCoreUseCase);
+        return new LoggingGameUseCaseDecorator(useCaseWithIdValidation);
+    }
 }
