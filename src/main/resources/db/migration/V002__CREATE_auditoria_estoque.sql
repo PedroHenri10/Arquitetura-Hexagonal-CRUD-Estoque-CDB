ffc499342746abf7a1ab9a87ac73a8bc8e1f6183
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-10-20 21:52:33 -0300

    Mudando MySQL

--- src/main/resources/db/migration/V002__CREATE_auditoria_estoque.sql
+++ src/main/resources/db/migration/V002__CREATE_auditoria_estoque.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS auditoria_estoque (
-  id SERIAL PRIMARY KEY,
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
   game_id BIGINT NOT NULL,
   estoque_antigo INT,
   estoque_novo INT,
   operacao VARCHAR(20),
   qtd_movimentada INT,
-  created_at TIMESTAMP DEFAULT NOW()
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_game_auditoria FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
 );
